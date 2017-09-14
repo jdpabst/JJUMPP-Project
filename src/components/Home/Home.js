bbf357e3a026a0e7b9d4f75ba0a1a8b8960941c2
Jessica <jdhpabst@gmail.com>
2017-09-14 10:54:26 -0600

    working on login errors

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,10 +7,11 @@ class Home extends Component {
   constructor(props){
       super(props);
       this.state = {
-        background: 'lime'
+        login: false
       };
       this.signInCallback = this.signInCallback.bind(this);
       this.login = this.login.bind(this);
+      this.handleLogin = this.handleLogin.bind(this);
     }
 
   signInCallback(authResult){
@@ -29,6 +30,19 @@ class Home extends Component {
       .then((res) => { console.log(res) })
   }
   componentDidMount(){
+    if(this.state.login){
+      document.getElementById('graphs').style.display = 'block'
+    } else {
+      document.getElementById('graphs').style.display = 'none'
+    }
+  }
+  handleLogin(){
+    console.log('fired')
+    if(!this.state.login){
+      this.setState({
+        login: true
+      })
+    }
   }
 
   render() {
@@ -40,13 +54,14 @@ class Home extends Component {
           <h1>HELLO!</h1>
           <div id="about_me">
             {/* <div id="me_pic"></div> */}
-            <p>Welcome to my app! Here, you can login with Google and navigate through some charts I made using the YouTube and Pinterest APIs. Enjoy!</p>
+            <p>Welcome to my app! Here, you can navigate to a Google login page to checkout some charts I made using the YouTube and Pinterest APIs. Enjoy!</p>
           </div>
           <div id="login_bttn">
             <div id="fb_icon"></div>
-            <a href="http://localhost:8087/auth/google" rel='external' >LOGIN WITH GOOGLE</a>
+            <a href="http://localhost:8087/auth/google" rel='external' onClick={this.handleLogin}>LOGIN WITH GOOGLE</a>
           </div>
         </div>
+        <div id="graphs">I AM A GRAPH</div>
       </div>
     );
   }
