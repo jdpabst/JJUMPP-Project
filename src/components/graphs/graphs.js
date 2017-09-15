7a656906d567db8508ff68871788d73d21d80b38
Jessica <jdhpabst@gmail.com>
2017-09-14 21:23:46 -0600

    adding new color to bar chart

--- src/components/graphs/graphs.js
+++ src/components/graphs/graphs.js
@@ -34,7 +34,8 @@ class Graphs extends Component {
         {name:"20 min vinyasa with a block",pv:227},
         {name:"20 Min Yin Yoga",pv:287},
         {name:"20 Minute Blindfolded Yoga Practice",pv:119}
-    ]
+    ],
+    list: []
   }    
 }
 
@@ -43,32 +44,39 @@ componentDidMount(){
   let arr = ['unINzOxPnow', '18DPn8SdkDE', 'BdTzZuwGEOw', '8zboEPyROUk', 'fWvvQh6XyHs', 'Pa-37deuCI8', 'h0Qeu8KX1BE', 'oHLC_Q_q4fw', 'ZTbF0hSXQXE', 'hiMeDj1jj5I']
   for(var i = 0; i < arr.length; i++){
     axios.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet%2C+statistics&id=${arr[i]}&maxResults=50&key=AIzaSyD-98pdn2HMAEmroyZrC-Fye_j24121uuE`).then((res) => {
-      list.push({name: res.data.items[0].snippet.title, pv: parseInt(res.data.items[0].statistics.likeCount)});
+      list.push({name: res.data.items[0].snippet.title, LIKES: parseInt(res.data.items[0].statistics.likeCount)});
       this.setState({
         videosArr: res.data.items[0].statistics,
+        list: list
     })
     })
   }
-  this.setState({
-    list: list
-  })
+  
 }
   render() {
     let videos = this.state.videosArr;
-    console.log(this.state.list);
+    let list = this.state.list;
+    let chart;
+    // console.log(this.state.list);
+    if(this.state.list.length >= 10){
+      chart = <BarChart className="bar_chart" width={800} height={400} data={this.state.list}>
+          <XAxis/>
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="LIKES" fill="#8884d8" />
+        </BarChart>
+      }
     return (
       <div className="graphs">
-          <h1>GRAPHS</h1>
-          <div>{ videos.likeCount }</div>
-          <div>{JSON.stringify(this.state.list)}</div>
-          <BarChart width={730} height={250} data={this.state.data}>
-            <XAxis dataKey="name" />
-            <YAxis />
-            <CartesianGrid strokeDasharray="3 3" />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="pv" fill="#8884d8" />
-          </BarChart>
+          <h1>YOGA BY CANDACE PLAYLIST 'LIKES' COUNT</h1>
+          {/* <div>{ videos.likeCount }</div> */}
+          {/* <div>{JSON.stringify(this.state.list)}</div> */}
+          {chart}
+          <ul id="list_list">{list.map((name, id) => {
+            return <li id="list_item">{ name.name }</li>
+          })}</ul>
       </div>
     );
   }
