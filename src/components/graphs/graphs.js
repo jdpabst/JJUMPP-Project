227493dc206561316b8e5a05d52a59b24f957b41
Jessica <jdhpabst@gmail.com>
2017-09-14 18:00:49 -0600

    got info from api

--- src/components/graphs/graphs.js
+++ src/components/graphs/graphs.js
@@ -10,24 +10,41 @@ class Graphs extends Component {
     this.state = {
       videosArr: [
         {
-          snippet:{},
-          statistics: {}
+          snippet:{
+            title: ''
+          },
+          statistics: {
+            commentCount:"",
+            dislikeCount:"",
+            favoriteCount:"",
+            likeCount:"",
+            viewCount:""
+          }
         }
       ]
   }    
 }
 
 componentDidMount(){
-  axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=6&order=relevance&q=yoga+by+candace&type=video&key=AIzaSyD-98pdn2HMAEmroyZrC-Fye_j24121uuE`).then(videosArr => {
+  let list = [];
+  let arr = ['unINzOxPnow', '18DPn8SdkDE', 'BdTzZuwGEOw', '8zboEPyROUk', 'fWvvQh6XyHs', 'Pa-37deuCI8', 'h0Qeu8KX1BE', 'oHLC_Q_q4fw', 'ZTbF0hSXQXE', 'hiMeDj1jj5I']
+  for(var i = 0; i < arr.length; i++){
+    axios.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet%2C+statistics&id=${arr[i]}&maxResults=50&key=AIzaSyD-98pdn2HMAEmroyZrC-Fye_j24121uuE`).then((res) => {
+      list.push({title: res.data.items[0].snippet.title, likes: res.data.items[0].statistics.likeCount});
       this.setState({
-          videosArr: videosArr.data.items,
+          videosArr: res.data.items[0].statistics,
+          list: list
       })
     })
   }
+}
   render() {
+    let videos = this.state.videosArr;
     return (
       <div className="graphs">
           <h1>GRAPHS</h1>
+          <div>{ videos.likeCount }</div>
+          <div>{JSON.stringify(this.state.list)}</div>
       </div>
     );
   }
